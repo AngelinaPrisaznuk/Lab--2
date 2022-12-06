0022179e3d5fc3a0888050019c2e77c63103c38f
AngelinaPrisaznuk <114239162+AngelinaPrisaznuk@users.noreply.github.com>
2022-12-06 19:50:24 +0200

    Add project files.

---
+++ Lab--2/Lab--2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    setlocale(0, "");
+
+    double a, b;
+
+    cout << "Exponentiate the number:" << endl; // Зведіть число до степеня 
+
+    cout << "Example: 5^2" << endl; //Наприклад: 5^2 
+
+    cout << "Enter a number:" << endl; //Введіть число 
+
+    cin >> a;
+
+    cout << "Enter the degree:" << endl; //Введіть степінь 
+
+    cin >> b;
+
+    cout << "Your answer = " << pow(a, b) << endl; //Ваша відповідь 
+
+    return 0;
+
+}
